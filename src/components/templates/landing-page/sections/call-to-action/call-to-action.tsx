0f498147da31c9f39f4c50b102effa1aa0d95612
Leonardo Fonteles <fonteles.leonardo@gmail.com>
2025-08-29 20:53:32 -0300

    finished refactored

--- src/components/templates/landing-page/sections/call-to-action/call-to-action.tsx
+++ src/components/templates/landing-page/sections/call-to-action/call-to-action.tsx
@@ -5,17 +5,15 @@ import Link from "next/link";
 export function CallToAction() {
   return (
     <section className="relative py-24 bg-gradient-to-b from-cyan-950/20 to bg-gray-700">
-      
-      <div className="bg-[url('/background-footer.svg')] absolute inset-0 bg-cover bg-center bg-no-repeat opacity-90"/>
-      
+      <div className="bg-[url('/background-footer.svg')] absolute inset-0 bg-cover bg-center bg-no-repeat opacity-90" />
+
+      <div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 bg-cyan-300 w-fit rounded-full hover:bg-cyan-900">
+        <Store className="text-cyan-100" />
+      </div>
+
       <div className="container relative">
         <div className="flex flex-col items-center gap-6 text-center">
-          <div className="p-4 bg-cyan-300 w-fit rounded-full hover:bg-cyan-900">
-            <Store className="text-cyan-100" />
-          </div>
-          <h2
-            className="font-sans text-gray-100 text-heading-lg text-balance"
-          >
+          <h2 className="font-sans text-gray-100 text-heading-lg text-balance">
             Crie uma loja online e inicie suas vendas ainda hoje
           </h2>
           <Button variant={"primary"} className="mt-6" asChild>
