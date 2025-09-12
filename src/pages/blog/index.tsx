e15e087be245364324a7ee958f6cffc04108609f
leonardo_fonteles <fonteles.leonardo@gmail.com>
2025-09-12 10:39:54 -0300

    added parameter in url with useRouter

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,5 @@
+import { Search } from "@/components/search";
+
 export default function BlogPage() {
     return (
         <div className="flex flex-col py-24 flex-grow h-full">
@@ -11,6 +13,8 @@ export default function BlogPage() {
                     </div>
                 </div>
             </header>
+            {/* Seach */}
+            <Search />
         </div>
     )
 }
