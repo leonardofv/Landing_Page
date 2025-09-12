e899351ebbd8c6499a09a1b7aedf5b003a34b512
leonardo_fonteles <fonteles.leonardo@gmail.com>
2025-09-12 11:39:14 -0300

    finished search component

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,13 +1,14 @@
+import { cn } from "@/lib/utils";
 import { SearchIcon } from "lucide-react";
 import { useRouter } from "next/router";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback } from "react";
 
 export function Search() {
 
     const router = useRouter();
-    const q = router.query.q as string;
-    const [query, setQuery] = useState('');
+    const query = router.query.q as string;
 
+    //enter
     const handleSearch = useCallback((event: React.FormEvent) => {
         event.preventDefault();
         if(query.trim()) {
@@ -15,17 +16,23 @@ export function Search() {
         }
     },[query, router]);
 
-    useEffect(() => {
-        setQuery(q);
-    },[q])
+    const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newQuery = event.target.value;
+
+        router.push(`/blog?q=${encodeURIComponent(newQuery)}`, undefined, {
+            shallow: true,
+            scroll: false,
+        });
+    }
 
     return (
-        <form onSubmit={handleSearch} className="relative">
-            <SearchIcon className="text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200" />
+        <form onSubmit={handleSearch} className="relative group">
+            <SearchIcon className={cn('text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300', query ? 'text-blue-300' : '')}
+            />
             <input 
                 type="text" 
-                placeholder="Buscar" 
-                onChange={(event) => setQuery(event.target.value)}
+                placeholder="Buscar"
+                onChange={handleQueryChange}
                 className="h-10 w-72 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md
                 text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300
                 focus-within:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm"
