1c87f19479eb444a9c070ff8ecb251811234ffb5
leonardo_fonteles <fonteles.leonardo@gmail.com>
2025-09-15 10:08:23 -0300

    finished search component and refatored page blog

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,12 +1,12 @@
 import { cn } from "@/lib/utils";
-import { SearchIcon } from "lucide-react";
+import { CircleX, SearchIcon } from "lucide-react";
 import { useRouter } from "next/router";
 import { useCallback } from "react";
 
 export function Search() {
 
     const router = useRouter();
-    const query = router.query.q as string;
+    const query = (router.query.q as string) ?? '';
 
     //enter
     const handleSearch = useCallback((event: React.FormEvent) => {
@@ -25,18 +25,27 @@ export function Search() {
         });
     }
 
+    const resetSearch = () => {
+        router.push(`/blog`, undefined, {
+            shallow: true,
+            scroll: false,
+        });
+    }
+
     return (
-        <form onSubmit={handleSearch} className="relative group">
+        <form onSubmit={handleSearch} className="relative group w-full md:w-60 border">
             <SearchIcon className={cn('text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300', query ? 'text-blue-300' : '')}
             />
             <input 
                 type="text" 
                 placeholder="Buscar"
+                value={query}
                 onChange={handleQueryChange}
-                className="h-10 w-72 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md
+                className="w-full h-10 md:w-60 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md
                 text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300
                 focus-within:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm"
             />
+            {query && (<CircleX onClick={resetSearch} className="text-gray-300 absolute w-4 h-4 top-1/2 -translate-y-1/2 right-3"/>)}
         </form>
     )
 }
