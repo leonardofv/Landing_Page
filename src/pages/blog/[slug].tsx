04f3c7ff0ea070fe1ae1e00c7b04f9edfef93235
leonardo_fonteles <fonteles.leonardo@gmail.com>
2025-10-01 11:31:49 -0300

    component Avatar

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -7,53 +7,71 @@ import {
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
-  BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-
+import { Avatar } from "@/components/avatar";
 
 export default function PostPage() {
+  const router = useRouter();
+  const slug = router.query.slug as string;
+  const post = allPosts.find((post) => post.slug.toLowerCase() == slug);
 
-    const router = useRouter();
-    const slug = router.query.slug as String;
-    const post = allPosts.find((post) => post.slug.toLowerCase().includes(slug.toLowerCase()));
+  const publishedDate = new Date(post?.date).toLocaleDateString('pt-BR');
 
-    return (
-        <main className="mt-10 text-gray-100">
-            <Breadcrumb>
-                <BreadcrumbList>
-                    <BreadcrumbItem>
-                        <BreadcrumbLink asChild className="text-action-sm">
-                            <Link href="/blog">
-                                Blog
-                            </Link>
-                        </BreadcrumbLink>
-                    </BreadcrumbItem>
+  return (
+    <main className="mt-10 text-gray-100">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild className="text-action-sm">
+              <Link href="/blog">Blog</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
 
-                    <BreadcrumbSeparator />
+          <BreadcrumbSeparator />
 
-                    <BreadcrumbItem>
-                        <BreadcrumbLink asChild>
-                            <Link href="">
-                                <span className="text-blue-200 text-action-sm">{post?.title}</span>
-                            </Link>
-                        </BreadcrumbLink>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-            </Breadcrumb>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="">
+                <span className="text-blue-200 text-action-sm">
+                  {post?.title}
+                </span>
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
 
-            <div className="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-6 lg:gap-12">
-                <article className="bg-gray-300 rounded-lg overflow-hidden border-gray-400 border-[1px]">
-                    <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
-                        <Image 
-                            src={post?.image ?? ''}
-                            alt={post?.title ?? ''}
-                            fill
-                            className="object-cover"
-                        />
-                    </figure>
-                </article>
-            </div>
-        </main>
-    )
+      <div className="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-6 lg:gap-12">
+        <article className="rounded-lg overflow-hidden border-gray-400 border-[1px]">
+          <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
+            <Image
+              src={post?.image ?? ""}
+              alt={post?.title ?? ""}
+              fill
+              className="object-cover"
+            />
+          </figure>
+          <header className="p-4 md:p-6 lg:p-12 pb-0">
+            <h1 className="mb-6 text-balance text-heading-lg md:text-heading-xl lg:text-heading-xl">
+              {post?.title}
+            </h1>
+
+            <Avatar.Container>
+                <Avatar.Image 
+                    src={post?.author.avatar}
+                    alt={post?.title}
+                />
+              <Avatar.Content>
+                <Avatar.Title>{post?.author.name}</Avatar.Title>
+                <Avatar.Description>
+                  <time dateTime={post?.date}>Publicado em {publishedDate}</time>
+                </Avatar.Description>
+              </Avatar.Content>
+            </Avatar.Container>
+          </header>
+        </article>
+      </div>
+    </main>
+  );
 }
--- src/templates/blog/components/post-card/post-card.tsx
+++ src/templates/blog/components/post-card/post-card.tsx
@@ -33,7 +33,7 @@ export function PostCard({ slug, image, date, title, description, author }: Post
                         alt=""
                         width={288}
                         height={144}
-                        className="w-full h-40 object-cover object-center rounded-t-[8px]"
+                        className="w-full h-40 object-cover object-center rounded-[8px]"
                     />
                 </div>
                 {/* Post info */}
