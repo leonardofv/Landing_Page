a428bce5dcabc3b56ec36c00028ebfaba442682f
leonardo fonteles <fonteles.leonardo@gmail.com>
2025-10-01 18:01:29 -0300

    component markdown

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -10,68 +10,72 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { Avatar } from "@/components/avatar";
+import { Markdown } from "@/components/markdown";
 
 export default function PostPage() {
   const router = useRouter();
   const slug = router.query.slug as string;
-  const post = allPosts.find((post) => post.slug.toLowerCase() == slug);
+  const post = allPosts.find((post) => post.slug.toLowerCase() == slug)!;
 
   const publishedDate = new Date(post?.date).toLocaleDateString('pt-BR');
 
   return (
-    <main className="mt-10 text-gray-100">
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild className="text-action-sm">
-              <Link href="/blog">Blog</Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-
-          <BreadcrumbSeparator />
-
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <Link href="">
-                <span className="text-blue-200 text-action-sm">
-                  {post?.title}
-                </span>
-              </Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
-
-      <div className="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-6 lg:gap-12">
-        <article className="rounded-lg overflow-hidden border-gray-400 border-[1px]">
-          <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
-            <Image
-              src={post?.image ?? ""}
-              alt={post?.title ?? ""}
-              fill
-              className="object-cover"
-            />
-          </figure>
-          <header className="p-4 md:p-6 lg:p-12 pb-0">
-            <h1 className="mb-6 text-balance text-heading-lg md:text-heading-xl lg:text-heading-xl">
-              {post?.title}
-            </h1>
-
-            <Avatar.Container>
-                <Avatar.Image 
-                    src={post?.author.avatar}
-                    alt={post?.title}
-                />
-              <Avatar.Content>
-                <Avatar.Title>{post?.author.name}</Avatar.Title>
-                <Avatar.Description>
-                  <time dateTime={post?.date}>Publicado em {publishedDate}</time>
-                </Avatar.Description>
-              </Avatar.Content>
-            </Avatar.Container>
-          </header>
-        </article>
-      </div>
-    </main>
+    <div className="container space-y-12 px-4 md:px-8">
+      <main className="mt-10 text-gray-100">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild className="text-action-sm">
+                <Link href="/blog">Blog</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link href="">
+                  <span className="text-blue-200 text-action-sm">
+                    {post?.title}
+                  </span>
+                </Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+        <div className="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-6 lg:gap-12">
+          <article className="rounded-lg overflow-hidden border-gray-400 border-[1px]">
+            <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
+              <Image
+                src={post?.image ?? ""}
+                alt={post?.title ?? ""}
+                fill
+                className="object-cover"
+              />
+            </figure>
+            <header className="p-4 md:p-6 lg:p-12 pb-0 mt-8 md:mt-12">
+              <h1 className="mb-8 text-balance text-heading-lg md:text-heading-xl lg:text-heading-xl">
+                {post?.title}
+              </h1>
+              <Avatar.Container>
+                  <Avatar.Image
+                      src={post?.author.avatar}
+                      alt={post?.title}
+                  />
+                <Avatar.Content>
+                  <Avatar.Title>{post?.author.name}</Avatar.Title>
+                  <Avatar.Description>
+                    <time dateTime={post?.date}>Publicado em {publishedDate}</time>
+                  </Avatar.Description>
+                </Avatar.Content>
+              </Avatar.Container>
+            </header>
+            <div className="px-4 mt-12 md:px-6 lg:px-12">
+              <Markdown
+                content={post.body.raw}
+              />
+            </div>
+          </article>
+        </div>
+      </main>
+    </div>
   );
 }
