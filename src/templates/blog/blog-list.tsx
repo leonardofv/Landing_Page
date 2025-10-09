713f68bdab33f576fbe6182f355a8360ad281f37
leonardo_fonteles <fonteles.leonardo@gmail.com>
2025-10-09 10:23:09 -0300

    applying ssg to the blog

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -2,21 +2,25 @@ import { Search } from "@/components/search";
 import { useRouter } from "next/router";
 import { PostCard } from "./components/post-card";
 import { PostGridCard } from "./components/post-grid-card/post-grid-card";
-import { allPosts } from "contentlayer/generated";
+import { Post } from "contentlayer/generated";
 import { Inbox } from "lucide-react";
 
-export function BlogList() {
+export type BlogListProps = {
+  posts: Post[];
+}
+
+export function BlogList({ posts }: BlogListProps) {
   const router = useRouter();
   const query = router.query.q as string;
   const pageTitle = query
     ? `Resultados de busca para "${query}"`
     : "Dicas e estratégias para impulsionar seu negócio";
 
-  const posts = query
-    ? allPosts.filter((post) =>
+  const postList = query
+    ? posts.filter((post) =>
         post.title.toLowerCase()?.includes(query.toLowerCase())
       )
-    : allPosts;
+    : posts;
 
   // const posts: Post[] = [];
   const hasPosts = posts.length > 0;
@@ -42,7 +46,7 @@ export function BlogList() {
       {/* Listagem dos posts */}
       {hasPosts && (
         <PostGridCard>
-          {posts.map((post) => (
+          {postList.map((post) => (
             <PostCard
               key={post._id}
               slug={post.slug}
