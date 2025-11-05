ffcb015e067a070af34ae0f4d1c48ea3591bc3cc
leonardo_fonteles <fonteles.leonardo@gmail.com>
2025-11-05 11:12:29 -0300

    Migrating landing page to app Router

--- src/components/active-link/active-link.tsx
+++ src/components/active-link/active-link.tsx
@@ -1,6 +1,7 @@
+'use client';
 import Link, { LinkProps } from "next/link";
 import { cn } from "@/lib/utils";
-import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 
 type ActiveLinkProps = {
     children: React.ReactNode;
@@ -8,16 +9,16 @@ type ActiveLinkProps = {
 
 export function ActiveLink({ children, href, ...rest  }: ActiveLinkProps ) {
 
-    const router = useRouter();
-    const isCurrentPath = 
-        router.asPath === href || 
-        router.asPath === rest.as;
+    const linkPath = (typeof href == 'string' ? href : href.pathname) ?? '';
+    const pathName = usePathname();
+    const isActive = pathName === linkPath || pathName?.startsWith(`${linkPath}/`);
 
     return (
        <Link 
+            {...rest}
             href={href} 
             className={
-                cn("text-action-sm transition-colors hover:text-blue-200", isCurrentPath ? 'text-blue-200' : 'text-gray-100')
+                cn("text-action-sm transition-colors hover:text-blue-200", isActive ? 'text-blue-200' : 'text-gray-100')
             }>{children}
         </Link> 
     )
